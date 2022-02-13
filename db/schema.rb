8ea0c0ed1a9f1a52ad8b9c26fb427383eb3cfc53
Chris Sloop <yaphetf@gmail.com>
2022-02-13 18:30:48 +0100

    replace with pg for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_02_12_142719) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "guesses", force: :cascade do |t|
     t.string "word", limit: 5, null: false
     t.string "player_id", null: false
