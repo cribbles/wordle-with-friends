1f012e62e460cded4d2c7eeb097e32472691bea4
Chris Sloop <yaphetf@gmail.com>
2022-03-12 11:56:27 +0100

    lint

--- app/javascript/controllers/player_name_controller.js
+++ app/javascript/controllers/player_name_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
-const ENTER = 13;
+const ENTER = 13
 const ANY_NON_WHITESPACE_CHAR_REGEXP = /\S/
 
 export default class extends Controller {
   static targets = ["name", "input"]
 
   click() {
-    this.nameTarget.textContent = "";
+    this.nameTarget.textContent = ""
     this.nameTarget.setAttribute("contenteditable", "true")
     this.nameTarget.focus()
   }
