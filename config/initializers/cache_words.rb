0cbbefb6d03177039adfcd98c4c31d04463d9dc8
Chris Sloop <csloop@wesleyan.edu>
2023-04-16 10:07:27 +0200

    feat: allow supplying secret word in development

--- config/initializers/cache_words.rb
+++ config/initializers/cache_words.rb
@@ -1,4 +1,15 @@
-words = File.readlines('./app/assets/cache/words.txt', chomp: true)
+secret_word = ENV["SECRET_WORD"] if Rails.env.development?
+if secret_word.present? && secret_word.size != 5
+  puts "Can't boot: SECRET_WORD needs to be five characters long"
+  exit(1)
+end
+
+valid_words = File.readlines('./app/assets/cache/words.txt', chomp: true)
+words = if secret_word.present?
+  [secret_word]
+else
+  valid_words
+end
 guesses = File.readlines('./app/assets/cache/guesses.txt', chomp: true)
 Rails.cache.write(:words, words)
-Rails.cache.write(:guesses, words + guesses)
+Rails.cache.write(:guesses, valid_words + guesses)
